8170f09c3c630db59c9f5c7e38cb0b953cfe78f8
Francisco José Fajardo Toril <frannnlol@gmail.com>
2017-04-18 19:48:47 +0200

    Moviendo funciones.
    Moviendo funciones de uso común a un fichero independiente.

--- inc/utilidades.h
+++ inc/utilidades.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include "infoBaseDatos.h"
 
 using namespace std;
 using namespace cv;
@@ -48,5 +49,61 @@ void marcar_punto(Mat &img, Point2f pt, Scalar& color, int tam_pixel);
 *	@brief Muestra por pantalla la versión de OpenCV que se tiene instalada.
 */
 void version_OpenCV();
+
+/**
+*	@fn getTrainingSample( const infoBaseDatos* _data_base )
+*	@brief This method calculates which sample element is in the training sample.
+*  @returns Vector indicating which element belongs to training (1) or not (0)
+*/
+vector<bool> getTrainingSample( const infoBaseDatos* _data_base, const float _training_sample_size = 0.7 );
+
+/**
+ * Returns the mirror image.
+ * @param _image Image to be flipped.
+ */
+Mat horizontalFlip(const Mat& _image);
+
+/**
+ * Makes a random zoom-in within a range.
+ * @param  _image    Image to be zoomed.
+ * @param  min_range Minimun percentage of the picture to be zoomed in.
+ * @param  max_range Maximun percentage of the picture to be zoomed in.
+ * @return           Zoomed in image.
+ */
+Mat randomZoom( const Mat& _image, int min_range = 10, int max_range = 30 );
+
+/**
+ * Makes a random rotation within a range.
+ * @param  _image    Image to be rotated.
+ * @param  min_range Minimun angle of the picture to be rotated.
+ * @param  max_range Maximun angle of the picture to be rotated.
+ * @return           rotated in image.
+ */
+Mat randomRotate( const Mat& _image, int min_range = 5, int max_range = 40 );
+
+/**
+ * It normalizes a image to reach 0 mean 1 variance. It is achieved by subtracting
+ * the image pixels mean and then dividing by image standard deviation.
+ * @param  _image image to be normalized.
+ * @return        Normalized image.
+ */
+Mat normalizeImage( const Mat& _image );
+
+/**
+ * Adds Gaussian Noise to a given image.
+ * @param  _image Image which will receive noise.
+ * @return        50% prob to return the given image with brigher or darker noise.
+ */
+Mat randomNoise( const Mat& _image );
+
+/**
+ * Apply a random combination of the transformations already implemented to the
+ * same image.
+ * @param  _image     Image to be transformed.
+ * @param  output_mat Transformed output image.
+ * @param  nt         Number of different transformations that can be applied.
+ * @return            True if some transformation was applied. False otherwise.
+ */
+bool combinedTrans( const Mat& _image, Mat& output_mat, int nt = 4 );
 
 #endif
--- src/utilidades.cpp
+++ src/utilidades.cpp
@@ -56,3 +56,161 @@ void version_OpenCV(){
  	cout << "Minor version : " << CV_MINOR_VERSION << endl;
 	cout << "Subminor version : " << CV_SUBMINOR_VERSION << endl;
 }
+
+
+vector<bool> getTrainingSample( const infoBaseDatos* _data_base, const float _training_sample_size ){
+	// Variables
+	vector<int> indexes;
+	vector<bool> output_vector;
+
+	// Method code
+	for( unsigned int i = 0; i < _data_base->get_num_sujetos(); i++ )
+		indexes.push_back( i );
+	random_shuffle( indexes.begin(), indexes.end() );
+
+	output_vector = vector<bool>( indexes.size(), false );
+	for( unsigned int i = 0; i < ceil( indexes.size() * _training_sample_size ); i++ )
+		output_vector.at( indexes.at( i ) ) = true;
+
+	return output_vector;
+}// getTrainingSample ends.
+
+
+Mat horizontalFlip(const Mat& _image){
+	Mat flipped_image;
+	flip( _image, flipped_image, 1 );
+	return flipped_image;
+}// horizontalFlip
+
+
+Mat randomZoom(const Mat& _image, int min_range, int max_range){
+ // Variables //
+ int x, y;
+ float zoom_range;
+ Mat zoomed_image;
+ Rect zoomed_area;
+
+ // Code //
+ // Defining a random zoom within a [min,max] range.
+ zoom_range = ( rand() % (max_range - min_range + 1) ) + min_range;
+ zoom_range /= 100;
+
+ // Defining the initial rect point.
+ x = _image.cols * zoom_range;
+ y = _image.rows * zoom_range;
+ zoomed_area = Rect( x, y, _image.cols - x, _image.rows - y );
+
+ // Zooming in while resizing the result image.
+ resize( _image( zoomed_area ), zoomed_image, _image.size() );
+
+ return zoomed_image;
+}// randomZoom.
+
+
+Mat randomRotate(const Mat& _image, int min_range, int max_range){
+  // Variables //
+  int aux;
+  Point2f center_point;
+  float rotation_angle;
+  Mat rot_matrix;
+  Mat rotated_image;
+
+  // Code //
+  // Calculating image center point.
+  center_point = Point2f( _image.cols/2.0F, _image.rows/2.0F);
+  // Getting the random rotation angle.
+  aux = rand();
+  rotation_angle = ( aux % (max_range - min_range + 1) ) + min_range;
+
+  if( aux % 2 == 0 )
+    rotation_angle *= -1;
+  // Getting the rotation matrix and applying it.
+  rot_matrix = getRotationMatrix2D( center_point, rotation_angle, 1.0 );
+  warpAffine( _image, rotated_image, rot_matrix, _image.size(),
+  CV_INTER_CUBIC,  BORDER_REFLECT);
+
+  return rotated_image;
+}// randomRotate
+
+
+Mat normalizeImage( const Mat& _image ){
+  // Variables
+  Mat _mean, _stddev, _temp;
+
+  // Code //
+  // Working on a temp variable. Changing its precision for better accuracy
+  _temp = _image.clone();
+  _temp.convertTo( _temp, CV_64F );
+
+  // Calculating mean and standard deviation.
+  meanStdDev( _temp, _mean, _stddev );
+
+  // Returning the normalized image.
+  return (_temp - _mean) / _stddev;
+}// normalizeImage
+
+
+Mat randomNoise( const Mat& _image ){
+  // Variables.
+  Mat gaussianNoise;
+
+  // Code //
+  gaussianNoise = _image.clone();
+  // Getting the mask.
+  randn(gaussianNoise ,50,20);
+
+  if( rand() % 2 == 0)
+    return _image - gaussianNoise; // Darker noise.
+  else
+    return _image + gaussianNoise; // Brighter noise.
+}
+
+
+/**
+ * Apply a random combination of the transformations already implemented to the
+ * same image.
+ * @param  _image     Image to be transformed.
+ * @param  output_mat Transformed output image.
+ * @param  nt         Number of different transformations that can be applied.
+ * @return            True if some transformation was applied. False otherwise.
+ */
+bool combinedTrans( const Mat& _image, Mat& output_mat, int nt ){
+  // Variables //
+  int trans_to_be_applied;
+  bool trasn_was_applied;
+
+  // Code //
+  output_mat = _image.clone();
+  trasn_was_applied = false;
+
+  // Applying 'num_trans' transformations with a 50% prob.
+  for( unsigned int i = 0; i < nt; i++ ){
+    trans_to_be_applied = rand() % nt;
+    if( rand() % 2 == 0 ){
+      trasn_was_applied = true;
+      switch ( trans_to_be_applied ) {
+        case 0:
+          output_mat = horizontalFlip( output_mat );
+        break;
+
+        case 1:
+          output_mat = randomZoom( output_mat );
+        break;
+
+        case 2:
+          output_mat = randomRotate( output_mat );
+        break;
+
+        case 3:
+          output_mat = randomNoise( output_mat );
+        break;
+
+        default:
+          cerr << "No hay definida una transfomación para el valor: " << trans_to_be_applied << endl;
+        break;
+      }
+    }
+  }
+
+  return trasn_was_applied;
+}
