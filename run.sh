7a3341b2af6c5c582caedb738370647121c489ca
Francisco José Fajardo Toril <frannnlol@gmail.com>
2017-05-04 18:15:09 +0200

    Actualizando script de ejecución

--- run.sh
+++ run.sh
@@ -14,19 +14,27 @@ fi
 
 cd $BUILD_DIR
 
-cmake .. && make
+cmake .. && make -q
+if [ $? -ne 0 ]; then
+  make
+fi
 
-case `echo $1 | tr '[:upper:]' '[:lower:]'` in
-  kdef)
-    echo "Preprocesando KDEF"
-    ./KDEF
-    ;;
-  yalefaces)
-    echo "Preprocesando Yalefaces"
-    ./yalefaces
-    ;;
-  *)
-    echo "Preprocesando KDEF"
-    ./KDEF
-    ;;
-esac
+if [ ! -f "trainingListFile.txt" ]; then
+  case `echo $1 | tr '[:upper:]' '[:lower:]'` in
+    kdef)
+      echo "Preprocesando KDEF"
+      ./KDEF
+      ;;
+    yalefaces)
+      echo "Preprocesando Yalefaces"
+      ./yalefaces
+      ;;
+    *)
+      echo "Preprocesando KDEF"
+      ./KDEF
+      ;;
+  esac
+  exit 0
+else
+  exit 1
+fi
